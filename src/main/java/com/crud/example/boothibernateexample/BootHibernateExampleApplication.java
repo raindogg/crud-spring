696defcc792b888c3456d81d79cf57d0b1fc3dfd
wabray17 <wabray17@gmail.com>
2018-05-25 15:28:29 -0500

    Files initated from spring initialzr

---
+++ src/main/java/com/crud/example/boothibernateexample/BootHibernateExampleApplication.java
@@ -0,0 +1,12 @@
+package com.crud.example.boothibernateexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootHibernateExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootHibernateExampleApplication.class, args);
+	}
+}
